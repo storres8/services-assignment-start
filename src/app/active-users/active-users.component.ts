b32af8b367623112575e4143ccf75ace374fc0a2
storres8 <stevetorres7562@gmail.com>
2019-07-03 15:16:08 -0400

    used the users service component to render and add active users.

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { UsersServiceService } from "../users-service.service";
 
 @Component({
-  selector: 'app-active-users',
-  templateUrl: './active-users.component.html',
-  styleUrls: ['./active-users.component.css']
+  selector: "app-active-users",
+  templateUrl: "./active-users.component.html",
+  styleUrls: ["./active-users.component.css"]
 })
 export class ActiveUsersComponent {
   @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+  // @Output() userSetToInactive = new EventEmitter<number>();
+
+  constructor(private usersService: UsersServiceService) {}
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    // this.userSetToInactive.emit(id);
+    this.usersService.onSetToInactive(id);
   }
 }
