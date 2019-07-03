fc11dbd3b74f96e9ae7b1a127c8cf94760819eba
storres8 <stevetorres7562@gmail.com>
2019-07-03 15:21:41 -0400

    rendered inactive users thorugh users service.

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { UsersServiceService } from "../users-service.service";
 
 @Component({
-  selector: 'app-inactive-users',
-  templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  selector: "app-inactive-users",
+  templateUrl: "./inactive-users.component.html",
+  styleUrls: ["./inactive-users.component.css"]
 })
 export class InactiveUsersComponent {
   @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+  // @Output() userSetToActive = new EventEmitter<number>();
+
+  constructor(private usersService: UsersServiceService) {}
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    // this.userSetToActive.emit(id);
+    this.usersService.onSetToActive(id);
   }
 }
